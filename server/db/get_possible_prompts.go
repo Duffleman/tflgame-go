c8d5d4c33f3f5f46573739f09b8a8134e35c965e
George Miller <git@duffleman.co.uk>
2020-10-24 10:14:52 +0100

    use a sub-query to prevent dupes

--- server/db/get_possible_prompts.go
+++ server/db/get_possible_prompts.go
@@ -5,13 +5,14 @@ import (
 	"strings"
 
 	"tflgame"
+	"tflgame/server/lib/cher"
 
 	sq "github.com/Masterminds/squirrel"
 )
 
 func (qw *QueryableWrapper) GetPossiblePrompts(ctx context.Context, options *tflgame.GameOptions, rounds int) ([]string, error) {
-	qb := NewQueryBuilder().
-		Select("DISTINCT(s.short_name), random()").
+	subQuery := NewQueryBuilder().
+		Select("DISTINCT(s.short_name)").
 		From("tfl_stops s").
 		LeftJoin("tfl_stops_zones zs on s.id = zs.stop_id").
 		Join("tfl_lines_stops ls ON s.id = ls.stop_id").
@@ -21,12 +22,15 @@ func (qw *QueryableWrapper) GetPossiblePrompts(ctx context.Context, options *tfl
 		})
 
 	if options.Zones != nil {
-		qb = qb.Where(sq.Eq{
+		subQuery = subQuery.Where(sq.Eq{
 			"zs.zone": options.Zones,
 		})
 	}
 
-	qb = qb.OrderBy("random()")
+	qb := NewQueryBuilder().
+		Select("t.short_name").
+		FromSelect(subQuery, "t").
+		OrderBy("random()")
 
 	if rounds > 0 {
 		qb = qb.Limit(uint64(rounds))
@@ -43,20 +47,25 @@ func (qw *QueryableWrapper) GetPossiblePrompts(ctx context.Context, options *tfl
 	}
 	defer rows.Close()
 
+	promptMap := map[string]struct{}{}
 	prompts := []string{}
 
 	for rows.Next() {
 		var p string
-		var r interface{}
 
-		err := rows.Scan(&p, &r)
+		err := rows.Scan(&p)
 		if err != nil {
 			return nil, err
 		}
 
 		p = strings.TrimSpace(p)
 
+		promptMap[p] = struct{}{}
 		prompts = append(prompts, p)
+	}
+
+	if len(prompts) != rounds {
+		return nil, cher.New("duplicates_generated", nil)
 	}
 
 	return prompts, nil
